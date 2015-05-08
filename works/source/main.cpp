d8d0ac998a11a998be3f7ffe3f1d62dafdda1b53
wuminye <huanlekf@163.com>
2015-05-08 21:31:05 +0800

    添加注释
    添加注释

--- works/source/main.cpp
+++ works/source/main.cpp
@@ -4,12 +4,27 @@
 
 void test()
 {
+    /*
+       一张牌的类Poker
+       两种构造函数
+       Poker a(ace,spade);
+        Poker a(12);
+    */
     Poker a(ace,club);
     Poker b(11);
     Poker c(10);
     Poker d(8);
     Poker e(9);
 
+/*
+   手牌类 HandCards
+   成员函数：
+       add(Poker) 添加扑克牌
+       GetClass() 获得类别名
+       GetUnique() 归类前排名
+       GetDistinct() 归类后排名
+
+*/
     HandCards t;
     t.add(a);
     t.add(b);
